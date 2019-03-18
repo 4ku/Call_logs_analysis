b85feaf5508c3cf7ad25e8e5e9ed4016c0790e65
4ku <ef.i.a@ya.ru>
2019-03-18 12:53:59 +0300

    updated

--- queries.sql
+++ queries.sql
@@ -17,24 +17,43 @@ CREATE TEMPORARY TABLE new_call_logs
 
 DROP TEMPORARY TABLE IF EXISTS charges;
 CREATE TEMPORARY TABLE charges
-select new_call_logs.Call_id, new_call_logs.From, new_call_logs.To, new_call_logs.UID_to, 
+select new_call_logs.Call_id, new_call_logs.From, new_call_logs.To, new_call_logs.UID_From, new_call_logs.UID_to, 
 	((UNIX_TIMESTAMP(Timestamp_end) - UNIX_TIMESTAMP(Timestamp_start))*0.04)  as charge from new_call_logs;
 
 update charges, numbers
 set charges.charge = 0
 where charges.UID_To = numbers.UID;
 
+-- Charges by each call
+select * from charges;
+ 
 -- Total charges
 select sum(charge) from charges;
 
+
+DROP TEMPORARY TABLE IF EXISTS in_UID;
+CREATE TEMPORARY TABLE in_UID 
+        select UID_To, count(UID_To) as c1 from new_call_logs
+		group by UID_To;
+
+DROP TEMPORARY TABLE IF EXISTS out_UID;
+CREATE TEMPORARY TABLE out_UID
+        select UID_From, count(UID_From) as c2 from new_call_logs
+		group by UID_From;
+        
 -- Top 10 users by income calls
-select UID_To, count(UID_To) from new_call_logs
-group by UID_To order by count(UID_To) desc Limit 10;
+select * from in_UID order by c1 desc Limit 10;
 
 -- Top 10 users by outcome calls
-select UID_From, count(UID_From) from new_call_logs
-group by UID_From order by count(UID_From) desc Limit 10;
+select * from out_UID order by c2 desc Limit 10;
 
+-- Top 10 'active' users
+select in_UID.UID_To, (in_UID.c1 + out_UID.c2) as total from in_UID , out_UID
+where in_UID.UID_To = out_UID.UID_From;
+
+-- Top-10 clients with highest charges
+select UID_From, sum(charge) from charges  where UID_From is not null
+	group by UID_From order by sum(charge) desc;
 
 
 
